21dd9420e739b6577baba985621e7c92ac6ceb34
FelipeMel0 <felipedemfernandes@gmail.com>
2022-03-14 11:23:08 -0300

    Testando Express e instalando Sequelize

---
+++ teste.js
@@ -0,0 +1,8 @@
+const express = require("express")
+const app = express()
+
+app.listen(4000, function(){
+    console.log("Servidor rodando na url http://localhost:4000")
+})
+
+//sequelize instalado
