ca11f1070432473e7eff70de3fd45288efa1871f
Felipe de Melo Fernandes <79909903+FelipeMel0@users.noreply.github.com>
2022-03-28 11:24:22 -0300

    Criando model de endereco

---
+++ models/usuarioComum/Endereco.js
@@ -0,0 +1,27 @@
+const db = require("../database/db")
+
+const UsuarioComum = require("./UsuarioComum")
+const Cidade = require("./Cidade")
+
+const Endereco = db.sequelize.define('tblEndereco', {
+
+    idEndereco: {
+        type: db.Sequelize.INTEGER, 
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+    },
+    cep: {
+        type: db.Sequelize.STRING(15),
+        allowNull: true
+    } 
+
+})
+
+UsuarioComum.hasMany(Endereco)
+
+Endereco.belongsTo(UsuarioComum)
+
+Cidade.hasMany(Endereco)
+
+Endereco.belongsTo(Cidade)
